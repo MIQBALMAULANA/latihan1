8da64dc0f08db57a592835c7b52528153481dc55
MIQBALMAULANA <56398951+MIQBALMAULANA@users.noreply.github.com>
2019-10-25 19:52:40 +0700

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,16 @@
+namalengkap = "M.Iqbal Maulana"
+namapanggilan = "Iqbal"
+NPM = "311910594"
+tempatlahir = "Kebumen"
+tanggallahir = "08, Noveber 2000"
+umur = "18"
+telepon = "081291211035"
+alamat = "Kp.Nagrak RT03/02 Desa Sukadami Kec.Cikarang Selatan Kab.Bekasi"
+
+print ("Assalamu'alaikum wr wb \n"
+       "Let me introduce My name is M.Iqbal Maulana, but you can call me Iqbal. My NPM IS \n"
+       "311910594. I was born in Kebumen and I am 18 years old. I am very glad if you want \n"
+       "to invite my house in Kp.Nagrak RT03/02 Desa Sukadami Kec.Cikarang Selatan Kab. \n"
+       "Bekasi. So, don't forget to call me before with the number 081291211035. \n"
+         
+        "Thanks you")
